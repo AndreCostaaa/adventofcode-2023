bf628055decee2a4b97b2e72858c26bd03fd3ac1
André Costa <toscmen.com@gmail.com>
2023-12-24 19:57:08 +0100

    day 5 part2

--- day5/main.py
+++ day5/main.py
@@ -1,6 +1,4 @@
-seeds = []
-
-maps_arr = [{} for _ in range(7)]
+import os
 
 
 def map_to_dict(line_arr, target_map):
@@ -26,12 +24,27 @@ def find_value_in_map(map, value):
     return value
 
 
+def find_value_in_map_reverse(map, value):
+    for line in map:
+        splitted = line.split(" ")
+        qty = int(splitted[2])
+        dst_range_start = int(splitted[0])
+        src_range_start = int(splitted[1])
+        delta = value - dst_range_start
+        if delta >= 0 and delta < qty:
+            return src_range_start + delta
+
+    return value
+
+
 lines = []
-with open("sample.txt", "r") as f:
+with open(
+    os.path.join(os.path.dirname(os.path.realpath(__file__)), "sample.txt"), "r"
+) as f:
     lines = f.readlines()
 
+seed_data = lines[0].split("seeds: ")[1].split(" ")
 
-seeds = [int(x) for x in lines[0].split("seeds: ")[1].split(" ")]
 
 maps = []
 on_map = False
@@ -46,12 +59,22 @@ for i, line in enumerate(lines[1:]):
     if on_map:
         maps[-1].append(line)
 
-locations = []
-for seed in seeds:
-    value = seed
-    for map in maps:
-        value = find_value_in_map(map, value)
+location_targets_map = sorted(maps[-1], key=lambda x: int(x.split(" ")[0]))
 
-    locations.append(value)
+maps.pop()
 
-print(min(locations))
+
+def find():
+    for location in range(1e8):
+        value = find_value_in_map_reverse(location_targets_map, location)
+        for map in reversed(maps):
+            value = find_value_in_map_reverse(map, value)
+
+        for i in range(0, len(seed_data), 2):
+            delta = value - int(seed_data[i])
+            if delta >= 0 and delta < int(seed_data[i + 1]):
+                print(location)
+                return
+
+
+find()
